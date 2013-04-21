ead44f88cfa4be9f771e084e15063021e0b9047a
Yair <ygraif@gmail.com>
2013-04-22 00:35:37 +0300

    Initiated Files
    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PharmacyApp::Application.config.secret_token = '874823e70ef800414c404f757e7169aeb2a0f2109f76a6d026bcbc39ae53cb9565a5c364fa5c5be32493ab2358a1c63a1d2d01f31afa5bd194a8cacc627ba7bd'
